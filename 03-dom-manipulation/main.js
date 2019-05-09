9d4444d36f26b5ed0bfc10d80c61fd50c88d00f1
Logesh Kumar <logesh.kumar@krds.fr>
2019-05-09 12:19:16 +0530

    Update MVC pattern

--- 03-dom-manipulation/main.js
+++ 03-dom-manipulation/main.js
@@ -1,40 +1,48 @@
 const model = {
   pocketSize: pocket.querySelectorAll('li').length,
-  A: [],
-  B: [],
-  C: []
+  A: {},
+  B: {},
+  C: {}
 }
 
 const view = {
   classifiedColumn: document.querySelectorAll('.classifiedColumn'),
   pocket: document.querySelector('#pocket'),
-  render: column => {
-    const index = column.charCodeAt() - 65;
+  render: value => {
+    const index = value[0].charCodeAt() - 65;
     const targetColumn = view.classifiedColumn[index];
-    targetColumn.querySelectorAll('li:not(:first-child').forEach(el => el.remove())
-    model[column].forEach(element => {
-      let li = document.createElement('li')
-      li.appendChild(document.createTextNode(element))
-      targetColumn.append(li)
-    });
+    const targetObj = model[value[0]];
+
+    if(targetColumn.querySelectorAll('li:not(:first-child').length) {
+      const referenceElement = targetColumn.querySelectorAll('li:not(:first-child)')[Object.keys(targetObj).indexOf(value)]
+      targetColumn.insertBefore(targetObj[value], referenceElement)
+    }
+    else {
+      targetColumn.append(targetObj[Object.keys(targetObj)[0]])
+    }
   },
   removeElement: target => {
     target.remove()
-    if(model.pocketSize <= 0) this.pocket.remove()
+  },
+  removePocket: () => {
+    this.pocket.remove()
   }
 }
 
 const controller = {
   pushData: function(target) {
-    const data = target.innerHTML;
-    model[data[0]].push(data)
+    const columnName = target.innerHTML[0];
+    model[columnName][target.innerHTML] = target;
     model.pocketSize--;
     view.removeElement(target);
-    this.sortData(data[0])
+    if(model.pocketSize <= 0) view.removePocket();
+    this.sortData(columnName)
+    view.render(target.innerHTML);
   },
-  sortData: column => {
-    model[column] = model[column].sort();
-    view.render(column);
+  sortData: columnName => {
+    const newObj = {};
+    Object.keys(model[columnName]).sort().forEach(key => newObj[key] = model[columnName][key])
+    model[columnName] = newObj;
   }
 }
 
