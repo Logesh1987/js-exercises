6117d24058b44185f64dde828e8ecf726c631dff
Logesh Kumar <logesh.kumar@krds.fr>
2019-06-10 10:34:14 +0530

    commit

--- 05-addition-triangle/main.js
+++ 05-addition-triangle/main.js
@@ -1,7 +1,10 @@
 const model ={
   triangleArray: [],
+  spacesArray: [],
   getTriangleArray: () => model.triangleArray,
   updateTriangleArray: (arr) => model.triangleArray.push(arr),
+  getSpacesArray: () => model.spacesArray,
+  updateSpacesArray: (val) => model.spacesArray.push(val),
   resetTriangle: () => model.triangleArray = []
 }
 
@@ -25,8 +28,8 @@ const controller = {
     const tempArr = []
 
     if(arr.length == 1) {
-      view.renderTriangle(model.getTriangleArray())
-      controller.createTriangle(model.getTriangleArray())
+      controller.createSpaces(model.getTriangleArray())
+      view.renderTriangle(model.getTriangleArray(), model.getSpacesArray())
       model.resetTriangle();
       return false
     }
@@ -36,21 +39,25 @@ const controller = {
     }
     controller.sumArray(tempArr)
   },
-  createTriangle : (arr) => {
+  createSpaces : (arr) => {
+    let temp = []
     let spaces = 0;
     for(let i = (arr.length - 1); i >= 0; i--) {
       let totalChar = arr[i].reduce((acc, cv) => cv.toString().length + acc, 0)
       if(spaces) {
-        const splitSpace = Math.ceil(spaces / (arr[i].length -1));
-        console.log(splitSpace);
+        temp.push(Math.ceil(spaces / (arr[i].length -1)))
       }
       spaces = totalChar + spaces
     }
+    console.log(temp, 'ppp');
+    temp = temp.reduce((acc, cv) => {
+      console.log(acc, cv);
+    })
   }
 }
 const view = {
   output: document.querySelector('.output'),
-  renderTriangle: (arr) => {
+  renderTriangle: (arr, spaces) => {
     view.output.innerHTML = '';
     let highestInt = arr.flat().pop().toString().length;
     let nbsp = new Array(highestInt + 1).join('&nbsp;')
@@ -59,6 +66,7 @@ const view = {
         let a = document.createElement('div');
         a.classList.add(`row_${index}`);
         view.output.insertBefore(a, document.querySelector(`.row_${index - 1}`))
+        // console.log(spaces[index]);
         arr.forEach((item, i) => {
           let b = document.createElement('span')
           b.innerHTML = item + nbsp;
